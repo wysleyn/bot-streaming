c1cb12e6cc9786a638b8c5f610702386c6b18db3
wysleyn <wysleyparaty@gmail.com>
2026-04-24 18:54:23 -0300

    Primeiro commit do bot

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("✅ Servidor do Bot está funcionando!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
