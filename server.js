59fc28ed7f33a95d257beceedcadf3fedb2997fa
wysleyn <wysleyparaty@gmail.com>
2026-04-24 19:13:12 -0300

    Adicionando rota webhook

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
 const PORT = process.env.PORT || 3000;
 
+// Rota principal
 app.get("/", (req, res) => {
-  res.send("✅ Servidor do Bot está funcionando!");
+  res.send("✅ Bot Streaming Online!");
+});
+
+// Rota para receber mensagens (Webhook)
+app.post("/webhook", (req, res) => {
+  const body = req.body;
+
+  console.log("Mensagem recebida:");
+  console.log(body);
+
+  res.status(200).send("Mensagem recebida com sucesso!");
 });
 
 app.listen(PORT, () => {
